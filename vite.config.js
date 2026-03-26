874f5a5cc5d46db608071b52ca8844da29fae1cb
rintuchowdory <chowdoryrintu60@gmail.com>
2026-03-26 18:31:54 +0100

    fix: set base to / for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/athan-app/' : '/',
-  server: { port: 4000, open: true },
+  base: '/',
+  server: { port: 4000 },
   build:  { outDir: 'dist', sourcemap: true },
 });
